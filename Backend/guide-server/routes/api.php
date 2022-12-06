12be46288ab4d85184d19febd90148432e5eb8be
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-12-06 23:47:31 +0200

    Initializing server

--- Backend/guide-server/routes/api.php
+++ Backend/guide-server/routes/api.php
@@ -2,18 +2,30 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BarController;
+use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\BeacheController;
+use App\Http\Controllers\CafeController;
+use App\Http\Controllers\RooftopController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["prefix" => "v0.1"], function() {
+	Route::group(["prefix" => "users"], function() {
+		
+	});
+    Route::group(["prefix" => "bars"], function() {
+		
+	});
+    Route::group(["prefix" => "restaurants"], function() {
+		
+	});
+    Route::group(["prefix" => "beaches"], function() {
+		
+	});
+    Route::group(["prefix" => "cafes"], function() {
+		
+	});
+    Route::group(["prefix" => "rooftops"], function() {
+		
+	});
 });
