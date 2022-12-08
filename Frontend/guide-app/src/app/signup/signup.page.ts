16973be68324f3c675423d96f6c1e35be10e6f9f
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-12-08 14:11:12 +0200

    Creating signup button

--- Frontend/guide-app/src/app/signup/signup.page.ts
+++ Frontend/guide-app/src/app/signup/signup.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router, private http: HttpClient) {}
+  goToHome() {
+    this.router.navigate(['/home']);
   }
-
+  ngOnInit() {}
 }
