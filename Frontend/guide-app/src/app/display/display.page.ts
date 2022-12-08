525fb3c94f89d5354ef5b1f3d3c3fd2e5bfd3dfe
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-12-08 18:35:55 +0200

    Creating display page

---
+++ Frontend/guide-app/src/app/display/display.page.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.page.html',
+  styleUrls: ['./display.page.scss'],
+})
+export class DisplayPage implements OnInit {
+  constructor(private router: Router, private http: HttpClient) {}
+  viewDetails() {
+    this.router.navigateByUrl('/details');
+  }
+  ngOnInit() {}
+}
