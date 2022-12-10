0f51aca2851cd8a01012c58f4202a70fb9c139fc
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-12-10 20:29:42 +0200

    Creating functions to save the id of the user in different tabs

--- Frontend/guide-app/src/app/tabs/tabs.page.ts
+++ Frontend/guide-app/src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.page.scss'],
 })
 export class TabsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public user: any = '';
+  public tmp_user: any = '';
+  public temp_user: any = '';
+  constructor(private router: Router) {}
+  goToAddReview() {
+    console.log(this.temp_user);
   }
-
+  goToAccount() {
+    console.log(this.temp_user);
+  }
+  ngOnInit() {
+    this.user = this.router.getCurrentNavigation()?.extras.state;
+    this.tmp_user = JSON.stringify(this.user);
+    this.temp_user = JSON.parse(this.tmp_user)['user'];
+    this.tmp_user = JSON.stringify(this.temp_user[0]);
+    this.temp_user = JSON.parse(this.tmp_user)['id'];
+  }
 }
