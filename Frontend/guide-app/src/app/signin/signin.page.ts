ea5d863b232e538ed675f6272ff2a5623d84a83f
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-12-10 15:06:56 +0200

    Creating function to send post request for login

--- Frontend/guide-app/src/app/signin/signin.page.ts
+++ Frontend/guide-app/src/app/signin/signin.page.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -8,9 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./signin.page.scss'],
 })
 export class SigninPage implements OnInit {
+  email: string = '';
+  password: string = '';
   constructor(private router: Router, private http: HttpClient) {}
+  checkUser() {
+    let postData = new FormData();
+    postData.append('email', JSON.stringify(this.email));
+    postData.append('password', JSON.stringify(this.password));
+    this.http
+      .post('http://127.0.0.1:8000/api/v0.1/users/login', postData)
+      .subscribe((data) => {
+        let tmp = JSON.stringify(data);
+        console.log(JSON.parse(tmp)['User']);
+      });
+  }
   goToSignup() {
-    this.router.navigate(['/signup']);
+    this.router.navigateByUrl('/signup');
   }
 
   ngOnInit() {}
