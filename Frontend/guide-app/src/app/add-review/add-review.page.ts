ca87d85bff2bfa6976317315ff532e33188fe1ed
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-12-12 00:08:17 +0200

    Creating link to add restaurant

--- Frontend/guide-app/src/app/add-review/add-review.page.ts
+++ Frontend/guide-app/src/app/add-review/add-review.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-add-review',
@@ -13,7 +14,11 @@ export class AddReviewPage implements OnInit {
   location: any = '';
   description: string = '';
   image: string = '';
-  constructor(private router: Router, private http: HttpClient) {}
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private storageService: StorageService
+  ) {}
   ionViewWillEnter() {
     let message = document.getElementById('addMessage') as HTMLInputElement;
     message.className = 'add';
@@ -31,6 +36,25 @@ export class AddReviewPage implements OnInit {
       message.className = 'add';
       message.innerHTML = 'Please Fill All Fields';
     } else {
+      this.storageService.get('ID').then((id) => {
+        let img = this.image.split('C:\\fakepath\\')[1];
+        let postData = new FormData();
+        postData.append('name', this.name);
+        postData.append('location', this.location);
+        postData.append('description', this.description);
+        postData.append('image', img);
+        postData.append('reviewed_by', id);
+        if (this.type === 'restaurant') {
+          this.http
+            .post(
+              'http://127.0.0.1:8000/api/v0.1/places/restaurants/addRestaurant',
+              postData
+            )
+            .subscribe(() => {
+              this.router.navigate(['/tabs']);
+            });
+        }
+      });
     }
   }
 
