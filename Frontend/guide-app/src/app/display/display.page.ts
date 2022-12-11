869d5e23b0f70179c2940eb4664956448976526f
MichelAD1 <michel.a.abidaoud@gmail.com>
2022-12-11 17:27:41 +0200

    Linking pages to display restaurants

--- Frontend/guide-app/src/app/display/display.page.ts
+++ Frontend/guide-app/src/app/display/display.page.ts
@@ -9,18 +9,46 @@ import { StorageService } from '../services/storage.service';
   styleUrls: ['./display.page.scss'],
 })
 export class DisplayPage implements OnInit {
+  array: any = [];
+  type: any = '';
   constructor(
     private router: Router,
     private http: HttpClient,
     private storageService: StorageService
   ) {}
-  viewDetails() {
+  viewDetails(place_id: any) {
+    console.log(place_id);
     this.router.navigateByUrl('/details');
   }
   goBack() {
     this.router.navigateByUrl('/tabs');
   }
-  ionViewWillEnter() {}
+  ionViewWillEnter() {
+    this.storageService.get('Place').then((place) => {
+      this.type = place;
+      if (place === 'Restaurant') {
+        this.http
+          .get(
+            'http://127.0.0.1:8000/api/v0.1/places/restaurants/getRestaurants'
+          )
+          .subscribe((data) => {
+            let tmp = JSON.stringify(data);
+            let temp = JSON.parse(tmp)['Restaurants'];
+            this.array = temp;
+          });
+      } else if (place === 'Cafe') {
+        console.log('Cafe');
+      } else if (place === 'Bar') {
+        console.log('Bar');
+      } else if (place === 'Beach') {
+        console.log('Beach');
+      } else if (place === 'Rooftop') {
+        console.log('rooftop');
+      } else if (place) {
+        console.log(place);
+      }
+    });
+  }
   ngOnInit() {
     this.ionViewWillEnter;
   }
